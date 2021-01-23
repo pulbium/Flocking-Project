a7849069f38210a32e49c81e60c2ac4106282f69
Adam Wiśniewski <adamwisiwmada@gmail.com>
2021-01-23 11:15:05 +0100

    Add project files.

---
+++ Flocking-Project/Boid.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Flocking_Project {
+	class Boid {
+		private double x, y, vx, vy, ax = 0, ay = 0;
+		static private double radius = 10, perceptionRadius = 50, perceptionAngle = 270;
+
+		public Boid(double x, double y, double vx, double vy) {
+			this.x = x;
+			this.y = y;
+			this.vx = vx;
+			this.vy = vy;
+		}
+
+		//odległość do punktu jako wektor przesunięcia
+		private double[] VecDistTo(double x, double y, bool toroidal, double maxX, double maxY) {
+			double dx = x - this.x;
+			double dy = y - this.y;
+
+			if (toroidal) {
+				if (dx > maxX / 2 || dx < -maxX / 2)
+					dx = maxX - dx;
+				if (dy > maxY / 2 || dy < -maxY / 2)
+					dy = maxY - dy;
+			}
+
+			double[] dist = { dx, dy };
+			return dist;
+		}
+
+		private double[] VecDistTo(Boid other, bool toroidal, double maxX, double maxY) {
+			return VecDistTo(other.x, other.y, toroidal, maxX, maxY);
+		}
+
+		//odległość do punktu jako wartość bezwzględna
+		private double DistTo(double x, double y, bool toroidal, double maxX, double maxY) {
+			double dx = Math.Abs(x - this.x);
+			double dy = Math.Abs(y - this.y);
+			if (toroidal) {
+				if (dx > maxX / 2)
+					dx = maxX - dx;
+				if (dy > maxY / 2)
+					dy = maxY - dy;
+			}
+			return Math.Sqrt(dx * dx + dy * dy);
+		}
+
+		private bool isInPercRadius(double x, double y, bool toroidal, double maxX, double maxY) {
+			//szukamy w kącie widzenia - liczymy kąt między wektorem prędkości a wektorem prowadzącym z naszego położenia do (x,y)
+			double[] r = VecDistTo(x, y, toroidal, maxX, maxY);
+			double product = vx * r[0] + vy * r[1];
+			product /= Math.Sqrt(vx * vx + vy * vy) * Math.Sqrt(r[0] * r[0] + r[1] * r[1]);
+
+			double angle = Math.Acos(product);
+
+			double dist = DistTo(x, y, toroidal, maxX, maxY);
+			if (dist < perceptionRadius && dist > 0 && angle < perceptionAngle / 2)
+				return true;
+			else
+				return false;
+		}
+
+		private bool isInPercRadius(Boid other, bool toroidal, double maxX, double maxY) {
+			return isInPercRadius(other.x, other.y, toroidal, maxX, maxY);
+		}
+
+		public void Borders(double maxX, double maxY, bool toroidal) {
+			if (toroidal) {
+				if (x < 0) x = maxX;
+				if (x > maxX) x = 0;
+				if (y < 0) y = maxY;
+				if (y > maxY) y = 0;
+			} else {
+				if (x < 0) {
+					x = 0;
+					vx *= -1;
+				}
+				if (x > maxX) {
+					x = maxX;
+					vx *= -1;
+				}
+				if (y < 0) {
+					y = 0;
+					vy *= -1;
+				}
+				if (y > maxY) {
+					y = maxY;
+					vy *= -1;
+				}
+			}
+		}
+
+		public void NormalizeVel(double maxSpeed) {
+			double len = Math.Sqrt(vx * vx + vy * vy);
+			vx /= len;
+			vy /= len;
+			vx *= maxSpeed;
+			vy *= maxSpeed;
+		}
+
+		public void NormalizeAcc(double maxAcc) {
+			double len = Math.Sqrt(ax * ax + ay * ay);
+			ax /= len;
+			ay /= len;
+			ax *= maxAcc;
+			ay *= maxAcc;
+		}
+
+		public void Separate(List<Boid> boids, double factor, bool toroidal, double maxX, double maxY) {
+			double steerX = 0, steerY = 0;
+			double[] dist;
+			foreach (Boid boid in boids) {
+				if (isInPercRadius(boid, toroidal, maxX, maxY)) {
+					dist = VecDistTo(boid, toroidal, maxX, maxY);
+					steerX -= dist[0];
+					steerY -= dist[1];
+				}
+			}
+			ax += steerX * factor;
+			ay += steerY * factor;
+		}
+
+		public void Align(List<Boid> boids, double factor, bool toroidal, double maxX, double maxY) {
+			double steerX = 0, steerY = 0;
+			foreach (Boid boid in boids) {
+				if (isInPercRadius(boid, toroidal, maxX, maxY)) {
+					steerX += boid.vx;
+					steerY += boid.vy;
+				}
+			}
+			ax += steerX * factor;
+			ay += steerY * factor;
+		}
+
+		public void Cohere(List<Boid> boids, double factor, bool toroidal, double maxX, double maxY) {
+			double steerX = 0, steerY = 0;
+			double[] dist;
+			foreach (Boid boid in boids) {
+				if (isInPercRadius(boid, toroidal, maxX, maxY)) {
+					dist = VecDistTo(boid, toroidal, maxX, maxY);
+					steerX += dist[0];   //dodajemy położenie sąsiedniego boidu względem tego boidu
+					steerY += dist[1];
+				}
+			}
+			ax += steerX * factor;
+			ay += steerY * factor;
+		}
+
+		public void ApplyAcc(double maxSpeed) {
+			double len = Math.Sqrt(ax * ax + ay * ay);
+			if (len > maxSpeed) {
+				ax /= len;
+				ay /= len;
+				ax *= maxSpeed;
+				ay *= maxSpeed;
+			}
+
+			vx += ax;
+			vy += ay;
+		}
+
+		#region setters/getters
+
+		public static double PA {
+			set { perceptionAngle = value; }
+			get => perceptionAngle; 
+		}
+
+		public double Ax {
+			set { ax = value; }
+		}
+
+		public double Ay {
+			set { ay = value; }
+		}
+
+		public double X {
+			set {
+				x = value;
+			}
+			get => x;
+		}
+
+		public double Y {
+			set {
+				y = value;
+			}
+			get => y;
+		}
+
+		public double Vx {
+			set {
+				vx = value;
+			}
+			get => vx;
+		}
+
+		public double Vy {
+			set {
+				vy = value;
+			}
+			get => vy;
+		}
+
+		public static double R {
+			set {
+				radius = value;
+			}
+			get => radius;
+		}
+
+		public static double PR {
+			set {
+				perceptionRadius = value;
+			}
+			get => perceptionRadius;
+		}
+		public double V {
+			get => Math.Sqrt(vx * vx + vy * vy);
+			
+		}
+
+		public double A {
+			get => Math.Sqrt(ax * ax + ay * ay);
+		}
+		#endregion
+	}
+}
---
+++ Flocking-Project/MainWindow.xaml.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Flocking_Project {
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window {
+		public MainWindow() {
+			InitializeComponent();
+
+			//żeby nie pisać tego 129576 razy
+			//listOfUIElements.Add(percRadCheckBox);
+			listOfUIElements.Add(percRLabel);
+			listOfUIElements.Add(percRadiusTxtBox);
+			listOfUIElements.Add(angleTextBox);
+			listOfUIElements.Add(angleLabel);
+			listOfUIElements.Add(amountTextBox);
+			listOfUIElements.Add(amountLabel);
+			listOfUIElements.Add(sepLabel);
+			listOfUIElements.Add(sepTxtBox);
+			listOfUIElements.Add(aliLabel);
+			listOfUIElements.Add(aliTxtBox);
+			listOfUIElements.Add(cohLabel);
+			listOfUIElements.Add(cohTxtBox);
+			listOfUIElements.Add(maxSpeedLabel);
+			listOfUIElements.Add(maxSpeedTextBox);
+			listOfUIElements.Add(toroidalCheckBox);
+			listOfUIElements.Add(resetBtn);
+
+			Boid.PR = double.Parse(percRadiusTxtBox.Text);
+			Boid.PA = double.Parse(angleTextBox.Text);
+
+			//kształt boida
+			boidPoints.Add(new Point(-2.5, -4));
+			boidPoints.Add(new Point(-2.5, 4));
+			boidPoints.Add(new Point(1.5, -2));
+			boidPoints.Add(new Point(2.5, -3));
+			boidPoints.Add(new Point(6, 0));
+			boidPoints.Add(new Point(2.5, 3));
+			boidPoints.Add(new Point(1.5, 2));
+
+			//domyślny kształt pola widzenia (ulegnie zmianie przy zmianie kąta)
+			percAreaPoints.Add(new Point(0, 0));
+			double N = 12;
+			double deltaAngle = Boid.PA / N;
+			for (int i = 0; i < N + 1; i++) {
+				double x = Boid.PR * Math.Cos((i * deltaAngle - Boid.PA / 2) * Math.PI / 180);
+				double y = Boid.PR * Math.Sin((i * deltaAngle - Boid.PA / 2) * Math.PI / 180);
+				Console.WriteLine(x + ", " + y);
+				percAreaPoints.Add(new Point(x, y));
+			}
+
+
+			dispatcherTimer.Tick += dispatcherTimer_Tick;
+			dispatcherTimer.Interval = new TimeSpan(0, 0, 0, 0, 10);
+			dispatcherTimer.Start();
+		}
+
+		System.Windows.Threading.DispatcherTimer dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
+		List<Boid> listOfBoids = new List<Boid>();
+		List<UIElement> listOfUIElements = new List<UIElement>();
+		bool animate = false;
+		double angle;
+		PointCollection boidPoints = new PointCollection();
+		PointCollection percAreaPoints = new PointCollection();
+		Random r = new Random();
+
+		private void DrawPercArea(Boid boid) {
+			angle = Math.Atan2(boid.Vy, boid.Vx);
+
+			Polygon percArea = new Polygon { Points = percAreaPoints, Stroke = Brushes.Blue, StrokeThickness = 1 };
+
+			percArea.RenderTransform = new RotateTransform(angle * 180 / Math.PI);
+			canvas.Children.Add(percArea);
+			Canvas.SetLeft(percArea, boid.X);
+			Canvas.SetTop(percArea, boid.Y);
+		}
+
+		private void DrawBoid(Boid boid) {
+			angle = Math.Atan2(boid.Vy, boid.Vx);
+
+			Polygon graphicalBoid = new Polygon { Points = boidPoints, Fill = Brushes.Red, Stroke = Brushes.Black, StrokeThickness = 1 };
+			canvas.Children.Add(graphicalBoid);
+			graphicalBoid.RenderTransform = new RotateTransform(angle * 180 / Math.PI);
+			Canvas.SetLeft(graphicalBoid, boid.X);
+			Canvas.SetTop(graphicalBoid, boid.Y);
+		}
+
+		private void dispatcherTimer_Tick(object sender, EventArgs e) {
+			if (animate) {
+				canvas.Children.Clear();
+				//liczymy nowe prędkości
+				for (int i = 0; i < listOfBoids.Count; i++) {   //for podobno szybszy od foreach
+					listOfBoids[i].Ax = 0;
+					listOfBoids[i].Ay = 0;
+					listOfBoids[i].Separate(listOfBoids, double.Parse(sepTxtBox.Text), (bool)toroidalCheckBox.IsChecked, canvas.Width, canvas.Height);
+					listOfBoids[i].Align(listOfBoids, double.Parse(aliTxtBox.Text), (bool)toroidalCheckBox.IsChecked, canvas.Width, canvas.Height);
+					listOfBoids[i].Cohere(listOfBoids, double.Parse(cohTxtBox.Text), (bool)toroidalCheckBox.IsChecked, canvas.Width, canvas.Height);
+				}
+				//i na podstawie prędkości nowe położenia
+				for (int i = 0; i < listOfBoids.Count; i++) {
+					listOfBoids[i].ApplyAcc(double.Parse(maxSpeedTextBox.Text));
+					if (listOfBoids[i].A > double.Parse(maxSpeedTextBox.Text))
+						listOfBoids[i].NormalizeAcc(double.Parse(maxSpeedTextBox.Text));
+					if (listOfBoids[i].V > double.Parse(maxSpeedTextBox.Text))
+						listOfBoids[i].NormalizeVel(double.Parse(maxSpeedTextBox.Text));
+					listOfBoids[i].X += listOfBoids[i].Vx;
+					listOfBoids[i].Y += listOfBoids[i].Vy;
+					listOfBoids[i].Borders(canvas.Width, canvas.Height, (bool)toroidalCheckBox.IsChecked);
+					if (percRadCheckBox.IsChecked == true)
+						DrawPercArea(listOfBoids[i]);
+					DrawBoid(listOfBoids[i]);
+				}
+			}
+		}
+
+
+		private void startStopBtn_Click(object sender, RoutedEventArgs e) {
+			if (startStopBtn.Content.Equals("START")) {
+				foreach (UIElement element in listOfUIElements) element.IsEnabled = false;
+				startStopBtn.Content = "STOP";
+				animate = true;
+			} else {
+				foreach (UIElement element in listOfUIElements) element.IsEnabled = true;
+				startStopBtn.Content = "START";
+				animate = false;
+			}
+		}
+
+		private void resetBtn_Click(object sender, RoutedEventArgs e) {
+			listOfBoids.Clear();
+			canvas.Children.Clear();
+			double x, y, angle;
+			for (int i = 0; i < int.Parse(amountTextBox.Text); i++) {
+				x = r.NextDouble() * canvas.Width;
+				y = r.NextDouble() * canvas.Height;
+				angle = r.NextDouble() * 2 * Math.PI;
+				Boid boid = new Boid(x, y, Math.Sin(angle), Math.Cos(angle));
+				listOfBoids.Add(new Boid(x, y, Math.Sin(angle), Math.Cos(angle)));
+				DrawBoid(boid);
+			}
+		}
+
+
+		private void angleTextBox_TextChanged(object sender, TextChangedEventArgs e) {
+			try {
+				Boid.PA = double.Parse(angleTextBox.Text);
+				//tworzymy nowe pole widzenia
+				percAreaPoints.Clear();
+
+				percAreaPoints.Add(new Point(0, 0));
+				double N = 12;
+				double deltaAngle = Boid.PA / N;
+				for (int i = 0; i < N + 1; i++) {
+					double x = Boid.PR * Math.Cos((i * deltaAngle - Boid.PA / 2) * Math.PI / 180);
+					double y = Boid.PR * Math.Sin((i * deltaAngle - Boid.PA / 2) * Math.PI / 180);
+					Console.WriteLine(x + ", " + y);
+					percAreaPoints.Add(new Point(x, y));
+				}
+			} catch (Exception) {
+				angleTextBox.Text = "270";
+				Boid.PA = 270;
+			}
+		}
+
+
+		private void amountTextBox_TextChanged(object sender, TextChangedEventArgs e) {
+			int amount;
+			double x, y, angle;
+
+			try {
+				amount = int.Parse(amountTextBox.Text);
+			} catch (Exception) {
+				amount = 50;
+				amountTextBox.Text = "50";
+			}
+
+			listOfBoids.Clear();
+			canvas.Children.Clear();
+
+			for (int i = 0; i < amount; i++) {
+				x = r.NextDouble() * canvas.Width;
+				y = r.NextDouble() * canvas.Height;
+				angle = r.NextDouble() * 2 * Math.PI;
+				Boid boid = new Boid(x, y, Math.Sin(angle), Math.Cos(angle));
+				listOfBoids.Add(boid);
+				DrawBoid(boid);
+			}
+		}
+
+		private void percRadiusTxtBox_TextChanged(object sender, TextChangedEventArgs e) {
+			try {
+				Boid.PR = double.Parse(percRadiusTxtBox.Text);
+
+				//tworzymy nowe pole widzenia
+				percAreaPoints.Clear();
+
+				percAreaPoints.Add(new Point(0, 0));
+				double N = 12;
+				double deltaAngle = Boid.PA / N;
+				for (int i = 0; i < N + 1; i++) {
+					double x = Boid.PR * Math.Cos((i * deltaAngle - Boid.PA / 2) * Math.PI / 180);
+					double y = Boid.PR * Math.Sin((i * deltaAngle - Boid.PA / 2) * Math.PI / 180);
+					Console.WriteLine(x + ", " + y);
+					percAreaPoints.Add(new Point(x, y));
+				}
+			} catch (Exception) {
+				percRadiusTxtBox.Text = "50";
+				Boid.PR = 50;
+			}
+		}
+	}
+}
